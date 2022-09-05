37aaed57d03b96fe96df49835a89c05cecdbc7e2
VanilStorm <vanil.work@mail.ru>
2022-09-05 21:12:21 +0300

    Added basic layout for main content

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,10 +1,47 @@
 import React, {FC} from 'react';
+import style from "./style.module.scss"
 
 const Main: FC = () => {
     return (
-        <main>
-           hellloasf
-        </main>
+            <table className={style.table}>
+                <thead>
+                    <tr>
+                        <th>Rank</th>
+                        <th>Name</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Bitcoin</td>
+                        <td>$30.000</td>
+                        <td>add</td>
+                    </tr>
+
+                    <tr>
+                        <td>1</td>
+                        <td>Bitcoin</td>
+                        <td>$30.000</td>
+                        <td>add</td>
+                    </tr>
+
+                    <tr>
+                        <td>1</td>
+                        <td>Bitcoin</td>
+                        <td>$30.000</td>
+                        <td>add</td>
+                    </tr>
+
+                    <tr>
+                        <td>1</td>
+                        <td>Bitcoin</td>
+                        <td>$30.000</td>
+                        <td>add</td>
+                    </tr>
+                </tbody>
+            </table>
     );
 };
 
