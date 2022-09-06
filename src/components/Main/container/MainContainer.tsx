9fa03afe2fad0ec7c90218bbf0856439ca527139
VanilStorm <vanil.work@mail.ru>
2022-09-06 22:49:52 +0300

    Added basic pagination

--- src/components/Main/container/MainContainer.tsx
+++ src/components/Main/container/MainContainer.tsx
@@ -1,10 +1,39 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import Main from "../layout/Main";
 import {useTypeSelector} from "../../../hooks/useTypeSelector";
 import {useActions} from "../../../hooks/useActions";
+import {IAllCoins} from "../../../types/allCoins";
 
 const MainContainer = () => {
     const {allCoins,loading} = useTypeSelector(state => state.AllCoinsReducer);
+    const [pageNum, setPageNum] = useState<number>(0);
+    const [coins, setCoins] = useState<IAllCoins[]>([]);
+
+    const coinsPerPage: number = 25;
+    const pageVisited: number = pageNum * coinsPerPage;
+
+    const displayCoinsPagination = coins.slice(pageVisited, pageVisited + coinsPerPage)
+
+    const handlePageChange = (e: React.ChangeEvent<HTMLButtonElement>): void => {
+        switch (e.target.name ) {
+            case 'next': {
+                if (pageNum === (allCoins.length / coinsPerPage) - 1) {
+                    return;
+                } else {
+                    setPageNum((num) => num + 1)
+                }
+                break;
+            }
+
+            case 'prev': {
+                if (pageNum === 0) {
+                    return;
+                } else {
+                    setPageNum((num) => num - 1)
+                }
+            }
+        }
+    }
 
     const {fetchAllCoins} = useActions();
 
@@ -12,13 +41,17 @@ const MainContainer = () => {
         fetchAllCoins();
     },[])
 
+    if (allCoins.length && !coins.length) {
+        setCoins(allCoins)
+    }
+
     if (loading) {
         return <h2>Loading</h2>
     }
 
     return (
         <>
-            <Main allCoins={allCoins} loading={loading}/>
+            <Main allCoins={displayCoinsPagination} handlePageChange={handlePageChange} pageNum={pageNum}/>
         </>
     );
 };
--- src/components/Main/layout/Main.tsx
+++ src/components/Main/layout/Main.tsx
@@ -4,34 +4,43 @@ import {IAllCoins} from "../../../types/allCoins";
 
 interface MainProps {
     allCoins: IAllCoins[],
-    loading: boolean,
+    handlePageChange: any,
+    pageNum: number,
 }
 
-const Main: FC <MainProps> = ({allCoins, loading}) => {
+const Main: FC <MainProps> = ({allCoins, handlePageChange, pageNum}) => {
 
     return (
-        <table className={style.table}>
-            <thead>
-            <tr>
-                <th>Rank</th>
-                <th>Name</th>
-                <th>Price</th>
-            </tr>
-            </thead>
+        <>
+            <table className={style.table}>
+                <thead>
+                <tr>
+                    <th>Rank</th>
+                    <th>Name</th>
+                    <th>Price</th>
+                </tr>
+                </thead>
 
-            <tbody>
-            {allCoins.map(item => {
-                return (
-                    <tr key={item.id}>
-                        <td>{item.rank}</td>
-                        <td>{item.name}</td>
-                        <td>$ {Number(item.priceUsd).toFixed(6)}</td>
-                        <td>Add</td>
-                    </tr>
-                )
-            })}
-            </tbody>
-        </table>
+                <tbody>
+                {allCoins.map(item => {
+                    return (
+                        <tr key={item.id}>
+                            <td>{item.rank}</td>
+                            <td>{item.name}</td>
+                            <td>$ {Number(item.priceUsd).toFixed(6)}</td>
+                            <td>Add</td>
+                        </tr>
+                    )
+                })}
+                </tbody>
+            </table>
+
+            <div className={style.buttons}>
+                <button onClick={handlePageChange} name='prev'>Prev</button>
+                <span><strong>{pageNum + 1}</strong></span>
+                <button onClick={handlePageChange} name='next'>Next</button>
+            </div>
+        </>
     );
 };
 
